c69e6d48bc4c6165fefe76dd66baf1117b71fd17
Veronics S <korolova27@rambler.ru>
2023-08-26 21:38:54 +0300

    pyproject2

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from funcs import sum_numb, dif_numb
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-sum_numb(6,5)
-dif_numb(9,3)
+        print(sum_numb(6,5))
+        dif_numb(9,3)
